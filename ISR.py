e12749d4600d0ef70abfa7cf4e85e4f80a1fe8cf
ss8319 <72968523+ss8319@users.noreply.github.com>
2022-03-23 18:42:27 +0800

    Image Super Resolution

---
+++ ISR.py
@@ -0,0 +1,32 @@
+#Installation
+#pip install ISR 
+#pip install 'h5py==2.10.0' --force-reinstall
+
+import numpy as np
+from PIL import Image
+
+
+img = Image.open('data/input/test_images/ISRtest.png')
+
+# Uncomment if using local repo
+# import sys
+# sys.path.append('..')
+from ISR.models import RDN, RRDN
+
+
+#model = RDN(weights='noise-cancel')
+#model = RRDN(weights='gans')
+#model = RDN(weights='psnr-small')
+model = RDN(weights='psnr-large')
+
+#Create Baseline
+bicubic_img=img.resize(size=(img.size[0]*2, img.size[1]*2), resample=Image.BICUBIC)
+
+#Save
+#bicubic_img = Image.fromarray(bicubic_img)
+bicubic_img.save('2_bicubic.png')
+
+#Prediction
+sr_img = model.predict(np.array(img))
+isr_img=Image.fromarray(sr_img)
+isr_img.save('2_psnr_large.png')
